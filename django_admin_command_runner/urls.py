6a82c10131e84d086880fa5929eb49c513d6c93b
Neels Kruger <neels.kruger@superbalist.com>
2025-07-31 14:09:40 +0200

    Run command change

--- django_admin_command_runner/urls.py
+++ django_admin_command_runner/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import CommandRunTerminalView
 from . import views
+
+app_name = "django_admin_command_runner"
+
 urlpatterns = [
     path("run-command/", views.run_command_view, name="run_command_view"),
     path("run-terminal/", CommandRunTerminalView.as_view(), name="run_command_view"),
